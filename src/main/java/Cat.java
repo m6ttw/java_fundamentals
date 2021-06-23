4de0eca4c155af1bf7b63bf814adb8b57ccf3328
MW <matthewrhyswilliams@live.com>
2021-06-23 14:55:35 +0100

    Create return String method

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,2 +1,5 @@
 public class Cat {
+    public String meow(){
+        return "Meowwww";
+    }
 }
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,6 @@
 public class Runner {
     public static void main(String[] args) {
         Cat myCat = new Cat();
-
+        System.out.println(myCat.meow());
     }
 }
