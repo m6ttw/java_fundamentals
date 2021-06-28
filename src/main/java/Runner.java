1d8bebb84eebdd08a66e3cbcf9a62aeb9979b82d
MW <matthewrhyswilliams@live.com>
2021-06-28 23:18:19 +0100

    Cat class has name

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,6 @@
 public class Runner {
     public static void main(String[] args) {
-        Cat myCat = new Cat();
+        Cat myCat = new Cat("Felix");
         System.out.println(myCat.meow());
     }
 }
