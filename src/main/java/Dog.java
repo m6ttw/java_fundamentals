777e1e9cbf9b9c3a9aeeb0a67754969467c466c9
MW <matthewrhyswilliams@live.com>
2021-06-28 23:19:37 +0100

    Dog class has name and bark method

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,2 +1,19 @@
 public class Dog {
+    private String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String woof(){
+        return "WOOF WOOF";
+    }
 }
